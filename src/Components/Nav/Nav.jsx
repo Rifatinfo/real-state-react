303aca823c0eb6a3c3ec302642b3d230e3b4dd15
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-03 17:53:57 +0600

    conditional avater image user name create

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
     ];
 
     const [open, setOpen] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     const toggleMenu = () => setOpen(!open);
 
@@ -37,24 +38,23 @@ const Navbar = () => {
                 {/* Search Bar - Hidden on small screens */}
                 <div className="flex justify-around gap-4">
                     {/* Dropdown Menu for Avatar */}
-                    <div className="dropdown dropdown-end">
+                    <div className="dropdown dropdown-end relative">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
                                 <img
+                                    onMouseEnter={() => setIsHovered(true)}
+                                    onMouseLeave={() => setIsHovered(false)}
                                     alt="Avatar"
                                     src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                             </div>
+                            {isHovered && (
+                                <span className="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 px-2 py-1 bg-gray-700 text-white text-sm rounded">
+                                    User Name
+                                </span>
+                            )}
                         </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">Profile</a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
                     </div>
+
                     <input
                         type="text"
                         placeholder="Search here"
