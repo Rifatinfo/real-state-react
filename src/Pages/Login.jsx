8ecdb88091d5f35d2bc67018d1548a93ade8331d
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-04 13:29:40 +0600

    login and register page correction

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -16,16 +16,16 @@ const Login = () => {
 
             {/* login details section  */}
             <div className="p-4 grid gap-7 grid-cols-1 lg:grid-cols-2 md:grid-cols-2 lg:max-w-6xl lg:mx-auto md:max-w-6xl md:mx-auto">
-                <div className="space-y-20 border">
+                <div className="space-y-20">
                     <div className="space-y-9">
                         <h1 className="text-4xl font-bold">Login to Bary</h1>
                         <p>Huge number of propreties availabe here for buy, sell and Rent.<br /> Also you find here co-living property, lots opportunity you have<br /> to choose here and enjoy huge discount you can get.</p>
                     </div>
-                    <div className="space-y-10 border">
+                    <div className="space-y-10 ">
                         <input className="font-light w-full sm:w-[400px] md:w-[350px] lg:w-[400px] leading-[1.75] placeholder:opacity-100 placeholder:text-body border-2 border-black border-opacity-60 rounded-[8px] p-[15px] focus:border-2  focus:border-[#ebcfa7] focus:border-opacity-60 focus:outline-none focus:drop-shadow-[0px_6px_15px_rgba(0,0,0,0.1)] " type="text" placeholder="email"></input><br />
                         <input className="font-light w-full sm:w-[400px]  md:w-[350px] lg:w-[400px] leading-[1.75] placeholder:opacity-100 placeholder:text-body border-2 border-black border-opacity-60 rounded-[8px] p-[15px] focus:border-2  focus:border-[#ebcfa7] focus:border-opacity-60 focus:outline-none focus:drop-shadow-[0px_6px_15px_rgba(0,0,0,0.1)] " type="password" placeholder="password"></input>
 
-                        <div className=" flex justify-between items-center border lg:justify-between lg:items-center 
+                        <div className=" flex justify-between items-center  lg:justify-between lg:items-center 
                                     lg:w-[400px] md:justify-between md:items-center">
                             <p><input type="checkbox" /> Remember me</p>
                             <p className="hover:under-line ">Forget password</p>
