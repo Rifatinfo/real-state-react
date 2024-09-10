14c2f4ad6de45f3c9f4fda4baaa2c80ea4e6912a
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-09-10 17:05:00 +0600

    active route set

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,17 +6,17 @@ const About = () => {
     return (
         <div>
             <Nav></Nav>
-            <div className="mt-32">
+            <div className="mt-32 bg-black bg-opacity-5 p-5">
                 <div>
                     <h1 className="lg:text-6xl text-center mb-4">About Bary.</h1>
-                    <p className="text-center text-sm">Huge number of propreties availabe here for buy<br /> and sell, also you can find here co-living property</p>
+                    <p className="text-center text-xs">Huge number of propreties availabe here for buy<br /> and sell, also you can find here co-living property</p>
                 </div>
                 <div className="mt-11 mx-w-6xl mx-auto">
-                    <img className="mx-auto w-72 lg:w-[597px] lg:h-auto" src={about_page} alt=""  width="597" height="716"/>
+                    <img className="mx-auto w-52 lg:w-[597px] lg:h-auto" src={about_page} alt=""  width="597" height="716"/>
                 </div>
                 <div className="mt-4">
                     <p className="text-center text-[#DDC7BB]">Since 1975</p>
-                    <h1 className="text-center lg:text-3xl font-semibold text-sm">We Provide Right Choice of Properties that You <br />need and have great opportunity to choose<br /> from thousands of Collection</h1>
+                    <h1 className="text-center lg:text-3xl font-semibold text-xs">We Provide Right Choice of Properties that You <br />need and have great opportunity to choose<br /> from thousands of Collection</h1>
                 </div>
                 {/* counting */}
                 <div className="flex justify-center space-x-8 text-center mt-8 mb-8">
@@ -24,19 +24,19 @@ const About = () => {
                         <h3 className="lg:text-4xl md:text-4xl font-bold">
                             <CountUp start={0} end={200} duration={5} />k+
                         </h3>
-                        <p className="md:text-lg lg:text-lg text-gray-600">Properties</p>
+                        <p className="md:text-lg lg:text-lg text-gray-600 text-sm">Properties</p>
                     </div>
                     <div className="stat-item">
                         <h3 className="lg:text-4xl md:text-4xl font-bold">
                             <CountUp start={0} end={120} duration={5} />k+
                         </h3>
-                        <p className="md:text-lg lg:text-lg text-gray-600">Customers</p>
+                        <p className="md:text-lg lg:text-lg text-gray-600 text-sm">Customers</p>
                     </div>
                     <div className="stat-item">
                         <h3 className="lg:text-4xl md:text-4xl font-bold">
                             <CountUp start={0} end={160} duration={5} />+
                         </h3>
-                        <p className="md:text-lg lg:text-lg text-gray-600">Awards Won</p>
+                        <p className="md:text-lg lg:text-lg text-gray-600 text-sm">Awards Won</p>
                     </div>
                 </div>
             </div>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,8 @@
 import { useContext, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { RiMenuUnfold2Fill } from "react-icons/ri";
 import { BiCross } from "react-icons/bi";
 import logo from '../../assets/logo-white.webp';
-import { Link } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 
 
@@ -30,7 +30,7 @@ const Nav = () => {
     const toggleMenu = () => setOpen(!open);
 
     return (
-        <div className="w-full fixed top-0 left-0 z-50 bg-transparent ">
+        <div className="w-full fixed top-0 left-0 z-50 bg-transparent">
             <div className="flex items-center justify-between py-4 px-7 md:px-10 relative">
                 {/* Logo */}
                 <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
@@ -41,7 +41,14 @@ const Nav = () => {
                 <ul className="hidden xl:flex items-center gap-8 font-semibold text-lg">
                     {Links.map((link, index) => (
                         <li key={index} className="text-[#ebcfa7] hover:text-white rounded-md p-2 transition-all cursor-pointer">
-                            <a href={link.link}>{link.name}</a>
+                            <NavLink
+                                to={link.link}
+                                className={({ isActive }) =>
+                                    isActive ? "text-white" : "text-[#ebcfa7]"
+                                }
+                            >
+                                {link.name}
+                            </NavLink>
                         </li>
                     ))}
                 </ul>
@@ -49,8 +56,8 @@ const Nav = () => {
                 {/* Search Bar - Hidden on small screens */}
                 <div className="flex justify-around gap-4">
                     {/* Dropdown Menu for Avatar */}
-                    {
-                        user ? <div className="dropdown dropdown-end relative">
+                    {user && (
+                        <div className="dropdown dropdown-end relative">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
                                     <img
@@ -65,28 +72,29 @@ const Nav = () => {
                                     </span>
                                 )}
                             </div>
-                        </div> : ""
-                    }
+                        </div>
+                    )}
 
-                    {/* login */}
-                    {
-                        user ?
-                            <button onClick={handleSignOut} className="btn btn-outline border-2 border-[#ebcfa7] text-[#ebcfa7] hover:bg-[#ebcfa7] hover:text-white hover:border-2 hover:border-[#ebcfa7] transition-all duration-200">
-                                SignOut
+                    {/* login/logout */}
+                    {user ? (
+                        <button
+                            onClick={handleSignOut}
+                            className="btn btn-outline border-2 border-[#ebcfa7] text-[#ebcfa7] hover:bg-[#ebcfa7] hover:text-white hover:border-2 hover:border-[#ebcfa7] transition-all duration-200"
+                        >
+                            SignOut
+                        </button>
+                    ) : (
+                        <Link to="/login">
+                            <button className="btn btn-outline border-2 border-[#ebcfa7] text-[#ebcfa7] hover:bg-[#ebcfa7] hover:text-white hover:border-2 hover:border-[#ebcfa7] transition-all duration-200">
+                                Login
                             </button>
-                            :
-                            <Link to="/login">
-                                <button className="btn btn-outline border-2 border-[#ebcfa7] text-[#ebcfa7] hover:bg-[#ebcfa7] hover:text-white hover:border-2 hover:border-[#ebcfa7] transition-all duration-200">
-                                    Login
-                                </button>
-                            </Link>
-                    }
-
+                        </Link>
+                    )}
 
                     <input
                         type="text"
                         placeholder="Search here"
-                        className="placeholder:text-[#ebcfa7] bg-transparent input input-bordered input-info w-full max-w-xs rounded-xl border-2 border-[#ebcfa7] focus:bg-slate-100 focus:outline-sky-500 hidden md:flex items-center gap-3 "
+                        className="placeholder:text-[#ebcfa7] bg-transparent input input-bordered input-info w-full max-w-xs rounded-xl border-2 border-[#ebcfa7] focus:bg-slate-100 focus:outline-sky-500 hidden md:flex items-center gap-3"
                     />
                 </div>
 
@@ -105,7 +113,14 @@ const Nav = () => {
                 </div>
                 {Links.map((link, index) => (
                     <li key={index} className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">
-                        <a href={link.link}>{link.name}</a>
+                        <NavLink
+                            to={link.link}
+                            className={({ isActive }) =>
+                                isActive ? "text-white" : "text-black"
+                            }
+                        >
+                            {link.name}
+                        </NavLink>
                     </li>
                 ))}
             </div>
